5dc3f09c42d8c220ec6e70726f3cca0337b76277
Tomaz Muraus <tomaz@tomaz.me>
2019-06-24 22:10:03 +0200

    Update script arguments.

--- scripts/run-datastore-emulator.sh
+++ scripts/run-datastore-emulator.sh
@@ -4,5 +4,5 @@
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
-nohup gcloud beta emulators datastore start --no-store-on-disk &
+sudo apt-get update && sudo apt-get install google-cloud-sdk google-cloud-sdk-datastore-emulator
+nohup gcloud beta emulators datastore start --host-port=127.0.0.1:8081 --no-store-on-disk &
